d5326739cac0222786fc225dd045adace85d3797
1101137126 <1101137126@gm.kuas.edu.tw>
2015-05-01 17:39:39 +0800

    homework555

---
+++ MvcApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using KuasCore.Models;
+using KuasCore.Services;
+
+namespace MvcApplication1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEmployeeService EmployeeService { get; set; }
+
+        [HttpGet]
+        public IList<Employee> GetAllEmployees()
+        {
+            return EmployeeService.GetAllEmployees();
+        }
+
+
+        [HttpGet]
+        [ActionName("byName")]
+        public Employee GetEmployeeById(string name)
+        {
+            var employee = EmployeeService.GetEmployeeByName(name);
+
+            if (employee == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return employee;
+        }
+
+        
+
+    }
+}
